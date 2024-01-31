4d58add5b76ed14914a7bc8952403e3d1c849ee3
Nikandrov Dmitry <dnikandrov@yandex.ru>
2024-02-01 01:20:49 +0300

    +2 func (titile,titile2)

--- str_func.py
+++ str_func.py
@@ -1,2 +1,9 @@
 def word_upper (word):
-    return word.upper
+    """Делает все буквы заглавными и только заглавными """
+    return word.upper()
+
+def word_title (word):
+    return word.title()
+
+def word_title2 (word):
+    return word.title()
